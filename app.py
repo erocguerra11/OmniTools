729e443f7c9ebbe56a6d94069b309a30bcb0852d
eric <eric2000gu@gmail.com>
2026-04-29 12:25:01 +0200

    Create app.py

---
+++ app.py
@@ -0,0 +1,19 @@
+from flask import Flask, render_template
+import os
+
+app = Flask(__name__)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/seguridad')
+def seguridad():
+    return render_template('seguridad.html')
+
+@app.route('/testers')
+def testers():
+    return render_template('testers.html')
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0', port=int(os.environ.get("PORT", 5000)))
